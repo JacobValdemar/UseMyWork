7068f6923ae773ce9b1db082911e247990550d99
JacobValdemar <JacobValdemarand@gmail.com>
2016-03-27 20:49:40 +0200

    Update cryptography.php

--- cryptography.php
+++ cryptography.php
@@ -96,4 +96,5 @@ echo openssl_decrypt($_POST["dec_text"], $_POST["dec_method"], $_POST["dec_passw
 		</td>
 	</tr>
 </table>
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Dataset" rel="dct:type">work</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Jacob Valdemar</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
 </html>
